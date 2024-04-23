7079b44a8293a1a60ccd4d83ce2e13387816820c
syntax-punk <david.jaeren@gmail.com>
2024-04-23 12:42:01 +0200

    Checnged function call sequence

--- public/app.js
+++ public/app.js
@@ -17,52 +17,55 @@ window.onload = function() {
 
     try {
       input = checkUrlStructure(input);
+
+      button.setAttribute('disabled', 'true');
+      spinner.classList.remove('hidden');
+  
+      fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: input })
+      }).then(response => {
+        if (response.ok) {
+          return response.blob();
+        }
+        throw new Error('Network response was not ok.');
+      }).then(blob => {
+        const objectUrl = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = objectUrl;
+        a.download = 'document.pdf';
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+      }).catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred. Please try again.');
+      }).finally(() => {
+        if (objectUrl) {
+          window.URL.revokeObjectURL(objectUrl);
+        }
+  
+        spinner.classList.add('hidden');
+        button.removeAttribute('disabled');
+      });
     } catch (error) {
       alert("We don't support that URL. Please try another one.");
     }
-
-    button.setAttribute('disabled', 'true');
-    spinner.classList.remove('hidden');
-
-    fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ url: input })
-    }).then(response => {
-      if (response.ok) {
-        return response.blob();
-      }
-      throw new Error('Network response was not ok.');
-    }).then(blob => {
-      const objectUrl = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = objectUrl;
-      a.download = 'document.pdf';
-      document.body.appendChild(a);
-      a.click();
-      a.remove();
-    }).catch(error => {
-      console.error('Error:', error);
-      alert('An error occurred. Please try again.');
-    }).finally(() => {
-      if (objectUrl) {
-        window.URL.revokeObjectURL(objectUrl);
-      }
-
-      spinner.classList.add('hidden');
-      button.removeAttribute('disabled');
-    });
   }
 }
 
 function checkUrlStructure(url) {
-  const protocol = url.split('://')[0];
+  const withProtocol = url.includes('://');
 
-  if (protocol.length) {
+  if (withProtocol) {
+    const protocol = url.split('://')[0].toLowerCase();
     if (protocol !== 'http' && protocol !== 'https')
       throw new Error('Invalid URL protocol');
+    
+    return url;
   } else {
     return `https://${url}`;
   }
